6e2befe2c296f173aab1f99aba2649f656889086
suzhenwei <su_zhenwei@yeah.net>
2017-09-21 10:38:53 +0800

    Add data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,154 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50626
+ Source Host           : localhost:3306
+ Source Schema         : latesummer
+
+ Target Server Type    : MySQL
+ Target Server Version : 50626
+ File Encoding         : 65001
+
+ Date: 21/09/2017 10:36:01
+*/
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`latesummer` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `latesummer`;
 
-DROP TABLE IF EXISTS `user`;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE TABLE `user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `name` varchar(20) DEFAULT NULL COMMENT '姓名',
-  `age` int(11) DEFAULT NULL COMMENT '年龄',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1029 DEFAULT CHARSET=utf8;
+-- ----------------------------
+-- Table structure for t_attach
+-- ----------------------------
+DROP TABLE IF EXISTS `t_attach`;
+CREATE TABLE `t_attach`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(100) NOT NULL,
+  `ftype` varchar(50) DEFAULT NULL,
+  `fkey` varchar(100) DEFAULT NULL,
+  `author_id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for t_comments
+-- ----------------------------
+DROP TABLE IF EXISTS `t_comments`;
+CREATE TABLE `t_comments`  (
+  `coid` int(10) NOT NULL AUTO_INCREMENT,
+  `cid` int(10) NOT NULL,
+  `created` int(10) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `author_id` int(10) DEFAULT 0,
+  `owner_id` int(10) DEFAULT 0,
+  `mail` varchar(100) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `ip` varchar(64) DEFAULT NULL,
+  `agent` varchar(255) DEFAULT NULL,
+  `content` text,
+  `type` varchar(32) DEFAULT NULL,
+  `status` varchar(32) DEFAULT NULL,
+  `parent` int(10) DEFAULT 0,
+  PRIMARY KEY (`coid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for t_contents
+-- ----------------------------
+DROP TABLE IF EXISTS `t_contents`;
+CREATE TABLE `t_contents`  (
+  `cid` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `thumb_img` varchar(255) DEFAULT NULL,
+  `created` int(10) NOT NULL,
+  `modified` int(10) DEFAULT NULL,
+  `content` text,
+  `author_id` int(10) NOT NULL,
+  `type` varchar(16) NOT NULL,
+  `STATUS` varchar(16) NOT NULL,
+  `fmt_type` varchar(16) DEFAULT 'markdown',
+  `tags` varchar(200) DEFAULT NULL,
+  `categories` varchar(200) DEFAULT NULL,
+  `hits` int(10) DEFAULT 0,
+  `comments_num` int(1) DEFAULT 0,
+  `allow_comment` int(1) DEFAULT 1,
+  `allow_ping` int(1) DEFAULT NULL,
+  `allow_feed` int(1) DEFAULT NULL,
+  PRIMARY KEY (`cid`) USING BTREE,
+  UNIQUE INDEX `cid`(`cid`) USING BTREE,
+  UNIQUE INDEX `slug`(`slug`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for t_logs
+-- ----------------------------
+DROP TABLE IF EXISTS `t_logs`;
+CREATE TABLE `t_logs`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `action` varchar(100) NOT NULL,
+  `data` varchar(2000) DEFAULT NULL,
+  `author_id` int(10) NOT NULL,
+  `ip` varchar(20) DEFAULT NULL,
+  `created` int(10) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `id`(`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for t_metas
+-- ----------------------------
+DROP TABLE IF EXISTS `t_metas`;
+CREATE TABLE `t_metas`  (
+  `mid` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `slug` varchar(200) DEFAULT NULL,
+  `type` varchar(32) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `sort` int(4) DEFAULT 0,
+  `parent` int(10) DEFAULT 0,
+  UNIQUE INDEX `mid`(`mid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for t_options
+-- ----------------------------
+DROP TABLE IF EXISTS `t_options`;
+CREATE TABLE `t_options`  (
+  `name` varchar(100) NOT NULL,
+  `VALUE` text,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`name`) USING BTREE,
+  UNIQUE INDEX `name`(`name`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for t_relationships
+-- ----------------------------
+DROP TABLE IF EXISTS `t_relationships`;
+CREATE TABLE `t_relationships`  (
+  `cid` int(10) NOT NULL,
+  `mid` int(10) NOT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for t_users
+-- ----------------------------
+DROP TABLE IF EXISTS `t_users`;
+CREATE TABLE `t_users`  (
+  `uid` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `home_url` varchar(255) DEFAULT NULL,
+  `screen_name` varchar(100) DEFAULT NULL,
+  `created` int(10) NOT NULL,
+  `activated` int(10) DEFAULT NULL,
+  `group_name` int(10) DEFAULT NULL,
+  PRIMARY KEY (`uid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
