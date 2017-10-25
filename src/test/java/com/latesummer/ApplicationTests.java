d293aeafc407bb74a5e1cdb7ad5e3917ea4e21b5
suzhenwei <su_zhenwei@yeah.net>
2017-10-25 10:44:30 +0800

    Bug:0000 Bugzilla Project: latesummer Description:

---
+++ src/test/java/com/latesummer/ApplicationTests.java
@@ -0,0 +1,41 @@
+package com.latesummer;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.latesummer.dao.UserRepository;
+import com.latesummer.model.entity.User;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(Application.class)
+public class ApplicationTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void test() throws Exception {
+        // 测试findAll, 查询所有记录
+        Assert.assertEquals(10, userRepository.findAll().size());
+
+        // 测试findByName, 查询姓名为FFF的User
+        Assert.assertEquals(60, userRepository.findByName("FFF"));
+
+        // 测试findUser, 查询姓名为FFF的User
+        Assert.assertEquals(60, userRepository.findUser("FFF").getAge().longValue());
+
+        // 测试findByNameAndAge, 查询姓名为FFF并且年龄为60的User
+        Assert.assertEquals("FFF", userRepository.findByNameAndAge("FFF", 60).getName());
+
+        // 测试删除姓名为AAA的User
+        userRepository.delete(userRepository.findByName("AAA"));
+
+        // 测试findAll, 查询所有记录, 验证上面的删除是否成功
+        Assert.assertEquals(9, userRepository.findAll().size());
+
+    }
+
+
+}
