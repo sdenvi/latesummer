470db8b08d6ea42e0e2fbe17886eb528f6c2acc7
suzhenwei <su_zhenwei@yeah.net>
2017-11-14 15:44:28 +0800

    Project: Latesummer Description:调整aop

--- src/main/java/com/latesummer/config/MyWebMvcConfigurerAdapter.java
+++ src/main/java/com/latesummer/config/MyWebMvcConfigurerAdapter.java
@@ -3,7 +3,9 @@ package com.latesummer.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import com.latesummer.interceptor.MyInterceptor;
+import com.latesummer.aop.MyInterceptor;
+import com.latesummer.aop.MyInterceptor2;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -21,8 +23,6 @@ import java.util.List;
  */
 @Configuration
 public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
-
-
     /**
      * 配置静态访问资源
      * @param registry
@@ -55,9 +55,11 @@ public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
      */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+    	// 多个拦截器组成一个拦截器链
         // addPathPatterns 用于添加拦截规则
         // excludePathPatterns 用户排除拦截
         //registry.addInterceptor(new MyInterceptor()).addPathPatterns("/**").excludePathPatterns("/toLogin","/login");
+    	//registry.addInterceptor(new MyInterceptor2()).addPathPatterns("/**").excludePathPatterns("/toLogin","/login");
         //super.addInterceptors(registry);
     } 
     
