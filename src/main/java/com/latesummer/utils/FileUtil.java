e000fe0cd9155fd19f19801a777bfe827346a11c
sdenvi <sdenvi@outlook.com>
2017-11-28 16:51:03 +0800

    Project: Latesummer Description:update

---
+++ src/main/java/com/latesummer/utils/FileUtil.java
@@ -0,0 +1,22 @@
+package com.latesummer.utils;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class FileUtil {
+
+	public static void uploadFile(byte[] file, String filePath, String fileName) throws Exception {
+		
+		//C:/Users/0200283/AppData/Local/Temp/tomcat-docbase.8921833096575767030.8080/upload
+		File targetFile = new File(filePath);
+		if (!targetFile.exists()) {
+			targetFile.mkdirs();
+		}
+		BufferedOutputStream out = new BufferedOutputStream(
+				new FileOutputStream(new File(filePath + fileName)));
+		out.write(file);
+		out.flush();
+		out.close();
+	}
+}
--- src/main/java/com/latesummer/web/controller/FileUploadController.java
+++ src/main/java/com/latesummer/web/controller/FileUploadController.java
@@ -1,13 +1,17 @@
 package com.latesummer.web.controller;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -104,4 +108,44 @@ public class FileUploadController {
 		}
 		return "upload successful";
 	}
+	
+	@RequestMapping("/download")
+    public String downLoad(HttpServletResponse response){
+        String filename="2.jpg";
+        String filePath = "F:/test" ;
+        File file = new File(filePath + "/" + filename);
+        if(file.exists()){ //判断文件父目录是否存在
+            response.setContentType("application/force-download");
+            response.setHeader("Content-Disposition", "attachment;fileName=" + filename);
+            
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null; //文件输入流
+            BufferedInputStream bis = null;
+            
+            OutputStream os = null; //输出流
+            try {
+                os = response.getOutputStream();
+                fis = new FileInputStream(file); 
+                bis = new BufferedInputStream(fis);
+                int i = bis.read(buffer);
+                while(i != -1){
+                    os.write(buffer);
+                    i = bis.read(buffer);
+                }
+                
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("----------file download" + filename);
+            try {
+                bis.close();
+                fis.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
 }
