e64333f2d61bed1f9118717000eed273e2448b8e
sdenvi <sdenvi@outlook.com>
2017-11-30 13:38:46 +0800

    Project: Latesummer Description:修复发送邮件的bug

--- src/test/java/com/latesummer/service/MailTests.java
+++ src/test/java/com/latesummer/service/MailTests.java
@@ -17,12 +17,12 @@ public class MailTests extends BasicUtClass{
 	
 	@Test
 	public void sendSimpleMail() {
-		mailService.sendSimpleMail(to, "主题：简单邮件", "测试邮件内容");
+		mailService.sendSimpleMail(to, "主题：简单邮件", "这是一封简单邮件！");
 	}
 	
 	@Test
 	public void sendAttachmentsMail() {
-		mailService.sendAttachmentsMail(to, "主题：带附件的邮件", "有附件，请查收！", "C:\\Users\\0200283\\Desktop\\上线流程文档准备.docx");
+		mailService.sendAttachmentsMail(to, "主题：带附件的邮件", "有附件，请查收！", "C:\\Users\\0200283\\Desktop\\SearchTableruleRQ.xsd");
 	}
 	
 	@Test
@@ -36,7 +36,7 @@ public class MailTests extends BasicUtClass{
 	}
 	
 	@Test
-	public void testHtmlMail() throws Exception {
+	public void sendHtmlMail() throws Exception {
 	    String content="<html>\n" +
 	            "<body>\n" +
 	            "    <h3>hello world ! 这是一封Html邮件!</h3>\n" +
