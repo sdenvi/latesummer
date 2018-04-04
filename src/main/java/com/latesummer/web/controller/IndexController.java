42967d0fa013dd1cb3e8e20294d4f150a5bf7da3
sdenvi <sdenvi@outlook.com>
2018-04-04 16:54:30 +0800

    update

--- src/main/java/com/latesummer/web/controller/IndexController.java
+++ src/main/java/com/latesummer/web/controller/IndexController.java
@@ -1,7 +1,24 @@
 package com.latesummer.web.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Maps;
+import com.latesummer.domain.LearnResouce;
+import com.latesummer.service.LearnService;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 主页
@@ -14,6 +31,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    @Autowired
+    private LearnService learnService;
+
     @RequestMapping("/main")
     public String main(){
         return "main";
@@ -22,6 +42,31 @@ public class IndexController {
     @RequestMapping("/index")
     public String index(){
         return "admin/index";
+    }
+
+    @RequestMapping("/admin_list")
+    public ModelAndView admin_list(HttpServletRequest request , HttpServletResponse response){
+        List<LearnResouce> learnList =new ArrayList<LearnResouce>();
+        //封装搜索参数
+        // 取得当前页数,注意这是jqgrid自身的参数
+        String page = request.getParameter("page");
+        // 取得每页显示行数，,注意这是jqgrid自身的参数
+        String rows = request.getParameter("rows");
+        String author = request.getParameter("author");
+        String title = request.getParameter("title");
+        Map<String, String> params = Maps.newHashMap();
+        params.put("author", author);
+        params.put("title", title);
+        //分页信息
+        Sort sort = new Sort(Sort.Direction.ASC, "id");
+        Pageable pageable = new PageRequest(NumberUtils.toInt(page, 1) - 1, NumberUtils.toInt(rows,10), sort);
+        Page<LearnResouce> rs = this.learnService.learnResouceListByPage(params, pageable);
+        ModelAndView modelAndView = new ModelAndView("/admin/admin-list");
+        modelAndView.addObject("rs", rs);
+        modelAndView.addObject("rows",  rs.getContent());
+        modelAndView.addObject("total",  rs.getTotalPages());
+        modelAndView.addObject("records",  rs.getTotalElements());
+        return modelAndView;
     }
 
     @RequestMapping("/welcome")
